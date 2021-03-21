a9a5c7c88835e412c7e1edad22962c48838e8e07
nicholas bezerra <nicholasbezerra1@gmail.com>
2021-03-21 17:43:01 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+	    
+		Scanner sc = new Scanner(System.in);
+		
+		String S1, S2, S3;
+		int x;
+		
+		x = sc.nextInt();
+		sc.nextLine();
+        S1 = sc.nextLine();
+		S2 = sc.nextLine();
+		S3 = sc.nextLine();
+		
+
+				
+		
+		System.out.println("Dados digitados: ");
+		System.out.println(x);
+		System.out.println(S1);
+		System.out.println(S2);
+		System.out.println(S3);
+		
+		sc.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
